f25370076347b07653a2f31bd86cd0093d0190a5
Immanuel Albrecht <github@genetic-superior.de>
2010-07-24 11:39:29 +0200

    Trying to make fresh master clone to work via ./bundle.sh "out-of-the-box"

--- bundle.sh
+++ bundle.sh
@@ -5,5 +5,6 @@ lein deps && lein jar && \
 mkdir -p conexp-clj/lib/ && \
 cp stuff/libs/*.jar conexp-clj/lib/ && cp stuff/libs/*.clj conexp-clj/lib && \
 cp -r bin lib res AUTHORS LICENSE README conexp-clj && \
+mv conexp-clj.jar conexp-clj-$(cat VERSION).jar && \
 mv conexp-clj-$(cat VERSION).jar conexp-clj/lib/conexp-clj-$(cat VERSION)-$(date +%Y%m%d.%H%M%S).jar && \
 zip -r conexp-clj.zip conexp-clj
